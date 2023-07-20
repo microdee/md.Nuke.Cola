b4e1ee7c9112737b776c2592591811009d385fad
David Mórász <david.morasz@realworld-one.com>
2023-07-20 14:02:10 +0200

    remove argument stuff from text extensions

--- md.Nuke.Cola/TextExtensions.cs
+++ md.Nuke.Cola/TextExtensions.cs
@@ -11,26 +11,6 @@ namespace Nuke.Cola;
 
 public static class TextExtensions
 {
-    public static string ProcessArgument(string arg)
-    {
-        if(string.IsNullOrWhiteSpace(arg)) return arg;
-
-        arg = arg.TrimMatchingDoubleQuotes()
-            .Replace("''", "\"") // sequence for double quotes
-            .Replace("~-", "-"); // sequence for -
-        if(arg[0] == '~')
-            arg = string.Concat("-", arg.AsSpan(1));
-        return arg;
-    }
-
-    public static IEnumerable<string> AsArguments(this IEnumerable<string> args) =>
-        args?.Select(ProcessArgument) ?? Enumerable.Empty<string>();
-
-    public static string AppendAsArguments(this IEnumerable<string> input, bool leadingSpace = true) =>
-        (input?.IsEmpty() ?? true)
-            ? ""
-            : (leadingSpace ? " " : "") + string.Join(' ', input?.Select(ProcessArgument) ?? Enumerable.Empty<string>());
-
     public static IEnumerable<string> DoubleQuoteIfNeeded(this IEnumerable<object> self) =>
         self?.Select(s => s.ToString().DoubleQuoteIfNeeded()) ?? Enumerable.Empty<string>();
 
