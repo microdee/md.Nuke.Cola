cdf7e5203b30754bc5e6bcaa371f373527a0b7c9
David Mórász <david.morasz@realworld-one.com>
2023-07-20 13:49:17 +0200

    xml docs

--- md.Nuke.Cola/BuildPlugins/Importable.cs
+++ md.Nuke.Cola/BuildPlugins/Importable.cs
@@ -12,7 +12,7 @@ namespace Nuke.Cola.BuildPlugins;
 /// <param name="Interface">.NET type of the build interface</param>
 /// <param name="Source">Original source path of the plugin (can be either a script or a project file)</param>
 /// <param name="ImportViaSource">
-/// When true the intermediate script will use `#load "<source>"` directive instead of `#r "<dll>"`
+/// When true the intermediate script will use `#load "&lt;source&gt;"` directive instead of `#r "&lt;dll&gt;"`
 /// when importing the build plugin. This was only intended to be used with C# script plugins.
 /// </param>
 public record Importable(Type Interface, AbsolutePath Source, bool ImportViaSource = false)
