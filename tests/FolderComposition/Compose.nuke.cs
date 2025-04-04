9599b6daf6cc45a128a81c26ab49f5391a26f643
David Morasz <m@mcro.de>
2025-04-04 21:44:27 +0200

    tests

--- tests/FolderComposition/Compose.nuke.cs
+++ tests/FolderComposition/Compose.nuke.cs
@@ -35,7 +35,7 @@ public interface IImportTestFolders : INukeBuild
                             ProcessContent = true
                         }
                     }
-                })
+                }, "test-export.yml")
             );
         });
 }
