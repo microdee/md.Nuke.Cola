e02b01d5dc0b06c12ddb474ea10b975b4507fcb3
David Morasz <m@mcro.de>
2025-08-28 12:41:15 +0200

    + With

--- md.Nuke.Cola/Cola.cs
+++ md.Nuke.Cola/Cola.cs
@@ -13,4 +13,23 @@ public static class Cola
     public static Dictionary<Key, Value> MakeDictionary<Key, Value>(params (Key key, Value value)[] items)
         where Key : notnull
         => items.ToDictionary(i => i.key, i => i.value);
+
+    /// <summary>
+    /// Simply invoke a task on an object and return that same object
+    /// </summary>
+    public static T With<T>(this T self, Action<T> operation)
+    {
+        operation(self);
+        return self;
+    }
+
+    /// <summary>
+    /// Simply invoke a task on an object which may be null and return that same object
+    /// </summary>
+    public static T? WithNullable<T>(this T? self, Action<T>? operation)
+    {
+        if (self != null)
+            operation?.Invoke(self);
+        return self;
+    }
 }
