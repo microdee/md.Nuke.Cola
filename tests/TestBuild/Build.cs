786f2c5de83fea0d80644fa2e568245f708269c5
David Morasz <m@mcro.de>
2025-09-16 01:46:19 +0200

    added test program for ToolEx features

--- tests/TestBuild/Build.cs
+++ tests/TestBuild/Build.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using Nuke.Cola;
@@ -10,6 +11,7 @@ using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
 using Serilog;
 using static Nuke.Common.EnvironmentInfo;
@@ -53,5 +55,32 @@ public class Build : NukeBuild
             XRepoTasks.Install(conanSpdlogSpec)("");
             var conanSpdlogInfo = XRepoTasks.Info(conanSpdlogSpec)("").ParseXRepoInfo();
             Log.Information("Linkdirs: {0}", conanSpdlogInfo[conanSpdlogSpec]?["fetchinfo"]?["linkdirs"]?.Value);
+        });
+
+    public Target BuildTestProgram => _ => _
+        .Executes(() =>
+        {
+            var testProgram = RootDirectory / "TestProgram";
+            DotNetTasks.DotNetBuild(c => c
+                .SetProjectFile(testProgram / "TestProgram.csproj")
+            );
+        });
+    
+    public Target TestToolEx => _ => _
+        .DependsOn(BuildTestProgram)
+        .Executes(() =>
+        {
+            var testProgramPath = RootDirectory / "TestProgram" / "bin" / "Debug" / "net9.0" / "TestProgram.exe";
+            var testProgram = ToolExResolver.GetTool(testProgramPath);
+
+            testProgram
+                .WithInput("Hello")
+                .WithInput("World")
+                .CloseInput()
+                ("input-length");
+
+            testProgram.WithInput(["jazz", "stuff"]).CloseInput()("repeat-line")!
+                .Pipe(testProgram)("repeat-line")!
+                .Pipe(testProgram)("repeat-line");
         });
 }
---
+++ tests/TestProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿switch (args)
+{
+    case ["repeat-line"]:
+        var input = Console.ReadLine();
+        Console.WriteLine(input + " again");
+        Console.WriteLine(input + " and again");
+    break;
+    
+    case ["input-length"]:
+        input = Console.In.ReadToEnd();
+        Console.WriteLine(input.Length);
+    break;
+    
+    default:
+        Console.WriteLine($"Wrong input: {string.Join(' ', args)}");
+    break;
+}
